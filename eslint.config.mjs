31eb0018e0238694402989c47513e0afd214c1ff
ahmedtaaw <ahmedtaaw@gmail.com>
2026-08-24 04:20:13 +0300

    chore: add jsx-a11y lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettierConfig from "eslint-config-prettier";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      ...jsxA11y.flatConfigs.recommended.rules,
+    },
+  },
   prettierConfig,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
     ".next/**",
     "out/**",
